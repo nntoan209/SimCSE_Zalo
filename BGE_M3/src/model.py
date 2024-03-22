9f4cc2291fe7921a98312a3eec11367eccf07f60
nntoan209 <nntoan209@gmail.com>
2024-03-23 03:05:14 +0700

    refactor src

---
+++ BGE_M3/src/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.distributed as dist
+from torch import nn, Tensor
+from typing import List, Tuple, Dict, Any, Optional
+
+class BGEM3Model(nn.Modelue):
+    pass
+
+
+class BGEM3ModelForInference:
+    pass
